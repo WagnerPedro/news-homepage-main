c91ea5f24de524d30e84085ee2ab1265965adab9
Wagner Pedro <wagnerpedro355@gmail.com>
2023-02-10 17:21:23 -0300

    bug do menu

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const $firstSpan = document.getElementById("firstSpan")
 const $body = document.getElementsByTagName("body")[0]
+const $menuIcon = document.querySelector(".menu-icon")
 
-let breakRow = document.createElement("span")
-breakRow.textContent = '/'
-breakRow.style.opacity = '0'
-$firstSpan.insertAdjacentElement("beforeend", breakRow)
+(() => {
+    let breakRow = document.createElement("span")
+    breakRow.textContent = '/'
+    breakRow.style.opacity = '0'
+    $firstSpan.insertAdjacentElement("beforeend", breakRow)
+})
+
+
+$menuIcon.style.display = 'flex'
+
+// (()=>{
+//     $menuIcon
+// })

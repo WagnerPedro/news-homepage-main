c3c256a184c362ecc3602f4cd6ca462239414baf
Wagner Pedro <wagnerpedro355@gmail.com>
2023-02-11 20:41:06 -0300

    danifiquei o menu

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-const $burguerMenu = document.querySelector(".burguerMenu")
-const menu = document.getElementsByTagName("menu")[0]
-const $menuImg = document.getElementById("imagem")
-const close = document.getElementById("close")
-const closeMenu =  document.querySelector(".closeMenu")
-
-$burguerMenu.addEventListener("click", ()=> {
-    if(!$menuImg.setAttribute("src", "./assets/images/icon-menu.svg")) {
-        menu.style.display = 'flex'
-        $menuImg.setAttribute("src", "./assets/images/icon-menu-close.svg")
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
     }
-    // console.log($menuImg)
-})
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
